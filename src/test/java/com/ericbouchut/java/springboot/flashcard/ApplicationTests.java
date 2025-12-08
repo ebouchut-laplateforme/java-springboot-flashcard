250818cc41f5dc9faac7f7e1bab158ed7977c0cd
Eric Bouchut <ebouchut@gmail.com>
2025-12-08 19:26:12 +0100

    fix(test): Add test profile to ApllicationTests

--- src/test/java/com/ericbouchut/java/springboot/flashcard/ApplicationTests.java
+++ src/test/java/com/ericbouchut/java/springboot/flashcard/ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ericbouchut.java.springboot.flashcard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Test
